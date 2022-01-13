87aa766d933b93cd6c184c7ee3735013d91c6e13
Moh-Khosravi <mohammad.khosravi@gmx.de>
2022-01-13 15:23:47 +0100

    sidebar v1

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
-
+import { MdOutlineDashboard } from 'react-icons/md'
+import { BsBookmarkPlus } from 'react-icons/bs'
+import { BsBookmarkStar } from 'react-icons/bs'
+import { FaUserFriends } from 'react-icons/fa'
+import { FiSettings } from 'react-icons/fi'
+import { FiLogIn } from 'react-icons/fi'
+import { FiLogOut } from 'react-icons/fi'
+import { useState } from 'react'
+import { GoCalendar } from 'react-icons/go'
+import SidebarBtn from './Sidebar-btn'
 function SideBar() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    function handleOpen () {
+        if (!isOpen) {
+            return (
+                <div className='btn-sideBar hover:bg-[#4A82F6]' onClick={() => setIsOpen(!isOpen)}>
+                    <div className='icon-sideBar'><FiLogIn /></div> 
+                    <div className='title-sideBar'>Log In</div>
+                </div>
+            )
+        } else {
+            return (
+                <div className='btn-sideBar hover:bg-[#4A82F6]' onClick={() => setIsOpen(!isOpen)}>
+                    <div className='icon-sideBar'><FiLogOut /></div> 
+                    <div className='title-sideBar'>Log Out</div>
+                </div>
+            )
+        }
+    }
     return (
-        <div>
-            
+        <div className='w-[212px] bg-[white] flex flex-col justify-between rounded-l-[35px] border'>
+            <div className='mt-[41px] ml-[37px] mb-[57px] text-[28px] '>LOGO</div>
+            <div className='btn-top'>
+                <SidebarBtn icon={<MdOutlineDashboard />} title='Dashboard' />
+                <SidebarBtn icon={<BsBookmarkPlus />} title='My Collection' />
+                <SidebarBtn icon={<BsBookmarkStar />} title='Favorites' />
+                <SidebarBtn icon={<GoCalendar />} title='Comming Soon' />
+                <SidebarBtn icon={<FaUserFriends />} title='Friends' />
+
+            </div>
+            <div className='btn-bottom'>
+                <SidebarBtn icon={<FiSettings />} title='Settings' />
+                {handleOpen()}
+            </div>
         </div>
     )
 }
---
+++ src/Components/Sidebar-btn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SidebarBtn({ icon, title}) {
+    return (
+        <div className='btn-sideBar hover:bg-[#4A82F6]'>
+            <div className='icon-sideBar'>{icon}</div>
+            <div className='title-sideBar'>{title}</div>
+        </div>
+    )
+}
+
+export default SidebarBtn
