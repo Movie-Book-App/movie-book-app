59bb4e4b005a815385d59996e5fb1d1fb4085681
a-langner <andreas_langner@yahoo.de>
2022-01-13 14:43:53 +0100

    Header-MockUp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { v4 as uuidv4 } from 'uuid'; // uuidv4();
+import Header from './Components/Header'
 
 function App() {
     return (
-        <div className='text-3xl font-bold underline'>
+        <div className='text-3xl'>
             hallo
+            <Header />
         </div>
     );
 }
