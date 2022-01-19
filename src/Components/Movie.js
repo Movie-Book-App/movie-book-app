0c4e40b6caa82a64b6b98fac9ef9cd5edc8fa2d9
zaki-coder <zakiazizi1841992@gmail.com>
2022-01-19 18:27:39 +0100

    finished trailer feature

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,9 +1,14 @@
-import React from "react"
+import React, {useState} from "react"
 import { v4 as uuidv4 } from "uuid" // uuidv4();
 import { MdOutlineStarBorderPurple500 } from "react-icons/md"
 import { AiFillHeart } from "react-icons/ai"
 import { useAppData } from "../Context/DataStorage"
+import YouTube from "react-youtube"
+import movieTrailer from "movie-trailer"
+
 function Movie(props) {
+    const { globalSearchString} = useAppData()
+    const [trailerUrl, setTrailerUrl ] = useState("")
     const { id, active, title, year, type, poster, runningTime, actors } =
         props.item
     const { onEdit } = useAppData()
@@ -24,12 +29,41 @@ function Movie(props) {
         return rhours + "h " + rminutes + "m"
     }
 
+    const opts = {
+        height: "250",
+        width: "400",
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters
+            autoplay: 1,
+        },
+    }
+
+    const handleClick = (movie) => {
+
+        if(trailerUrl) {
+            setTrailerUrl("")
+        } else {
+            movieTrailer(title || "")
+            .then((url) => {
+                console.log(url)
+                const urlParams = new URLSearchParams(new URL(url).search)
+                
+                setTrailerUrl(urlParams.get("v"))
+            })
+                .catch((error) => {
+                    console.log(error)
+                })
+        }
+    }
+
     return (
         <div
-            key={uuidv4()}
+            
             className="flex items-center p-4 border-2 border-gray-200 rounded-lg shadow-sm bg-gray-800 h-[300px]"
         >
             <img
+                key={id}
+                onClick={() => handleClick(id)}
                 src={poster}
                 alt=""
                 className="w-1/5 rounded-md bg-cover h-full bg-center object-scale-down"
@@ -37,7 +71,11 @@ function Movie(props) {
             <div className="flex flex-col ml-5 h-full w-4/5">
                 <div className="flex text-white justify-between">
                     <p className="text-xl font-semibold mb-2">Title: {title}</p>
-                    <span>{isNaN(runningTime)?"not available" : timeConvert(runningTime)}</span>
+                    <span>
+                        {isNaN(runningTime)
+                            ? "not available"
+                            : timeConvert(runningTime)}
+                    </span>
                 </div>
                 <p className="mt-2 text-white">Type: {type}</p>
                 <p className="mt-3 text-white">
@@ -59,6 +97,7 @@ function Movie(props) {
                     </div>
                 </div>
             </div>
+            {trailerUrl && <YouTube className="ml-3 rounded-xl" videoId={trailerUrl} opts={opts} />}
         </div>
     )
 }
